170b7c6af00d9b8cebc6f08ea9fb79b6ca9f0408
kentana20 <tanakak@eac.ne.jp>
2016-05-02 14:40:22 +0900

    added calendar

---
+++ Def-Swift/1-swift-programming/calendar.swift
@@ -0,0 +1,22 @@
+let days = 31
+let firstDay = 2
+var w = 0
+
+for ; w < firstDay; w++ {
+    print("    ", terminator:"")
+}
+
+var d = 1
+loop: while true {
+    for ; w < 7; w++ {
+        let pad = d < 10 ? " " : ""
+        print(pad + "  \(d)", terminator:"")
+        if ++d > days {
+            print("")
+            break loop
+        }
+    }
+    print("")
+    w=0
+}
+
