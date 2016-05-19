02799b27cbe41227248cdfd334594c7f8bb57a04
kentana20 <tanakak@eac.ne.jp>
2016-05-19 09:06:29 +0900

    added protocol extension

--- Def-Swift/11-extension/datable.swift
+++ Def-Swift/11-extension/datable.swift
@@ -4,3 +4,22 @@ protocol Datable {
     var day: Int { get }
 }
 
+// ツェラーの公式
+func Zeller(var m:Int, _ d:Int, var _ y:Int) -> Int {
+     if m < 3 {
+          m += 12; --y
+     }
+     let leap = y + y / 4 - y / 100 + y / 400
+     return (leap + (13 * m + 8) / 5 + d) % 7
+}
+
+extension Datable {
+    var dayOfWeek: Int {
+        return Zeller(month, day, year)
+    }
+}
+
+struct Date : Datable {
+    var year, month, day: Int
+}
+
